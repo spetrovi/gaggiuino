28cfa09bde52cb0ccea627e9a527bc408186d3e0
spetrovi <spetrovi@redhat.com>
2022-12-09 23:35:29 +0100

    Cleanup

--- src/peripherals/thermocouple.h
+++ src/peripherals/thermocouple.h
@@ -3,23 +3,18 @@
 
 #include "pindef.h"
 
-#if defined SINGLE_BOARD
-  #include <Adafruit_MAX31865.h>
-  Adafruit_MAX31865 thermocouple = Adafruit_MAX31865(thermoCS);
-#endif
+#include <Adafruit_MAX31865.h>
+Adafruit_MAX31865 thermocouple = Adafruit_MAX31865(thermoCS);
 
 static inline void thermocoupleInit(void) {
-#if defined SINGLE_BOARD
-  thermocouple.begin(MAX31865_2WIRE);
-#endif
+thermocouple.begin(MAX31865_2WIRE);
 }
 
 static inline float readCelsius(void) {
-#if defined SINGLE_BOARD  
-  #define RREF      430.0
-  #define RNOMINAL  100.0  
-  return thermocouple.temperature(RNOMINAL, RREF);
-#endif
+#define RREF      430.0
+#define RNOMINAL  100.0  
+return thermocouple.temperature(RNOMINAL, RREF);
+
 }
 
 #endif
